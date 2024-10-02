e042363d93f30c21cc7f30fa159a93aede4e2c80
N1Chaos <sagiadinos@wanadoo.fr>
2024-10-02 18:41:19 -0400

    message

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:uFXVVctXAYIcQyE3NKlMwCdE@172.21.107.207:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
